4b5e8c1410f5eeaf4662a959c0e0b424524dffac
Philip Gausaker <philisg@stud.ntnu.no>
2021-02-09 15:41:40 +0100

    added channels and buffer

--- 3 - bounded buffer/go/main.go
+++ 3 - bounded buffer/go/main.go
@@ -5,22 +5,24 @@ import "fmt"
 import "time"
 
 
-func producer(/*TODO: parameters?*/){
+func producer(x chan<- int){
 
     for i := 0; i < 10; i++ {
         time.Sleep(100 * time.Millisecond)
         fmt.Printf("[producer]: pushing %d\n", i)
         // TODO: push real value to buffer
+		x <- i
     }
+	
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(x <-chan int){
 
     time.Sleep(1 * time.Second)
     for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
+        //i <- x//TODO: get real value from buffer
+        fmt.Printf("[consumer]: %d\n", <-x)
         time.Sleep(50 * time.Millisecond)
     }
     
@@ -30,9 +32,10 @@ func consumer(/*TODO: parameters?*/){
 func main(){
     
     // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
+    buffer := make(chan int, 5)
+	
+    go consumer(buffer)
+    go producer(buffer)
     
     select {}
 }
